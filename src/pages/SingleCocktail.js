68b49643480884fe46c93281026837585bb914d6
hernanguaragna <hernanguaragna@gmail.com>
2022-11-29 16:07:55 -0300

    cambio de mensaje si no encuentra cocktail

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -60,7 +60,7 @@ export default function SingleCocktail() {
     return <Loading/>
   }
   if (!cocktail) {
-    return <h2 className='section-title'>no cocktail to display</h2>
+    return <h2 className='section-title'>no hay cocktails para mostrar</h2>
   } else {
     const {
       name,
